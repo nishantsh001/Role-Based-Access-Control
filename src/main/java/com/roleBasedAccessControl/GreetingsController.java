686892374246944cb2a81633342c5938564bbdfa
nishantsh001 <nishant.sh1999@gmail.com>
2024-11-28 00:54:46 +0530

    2nd day commit

--- src/main/java/com/roleBasedAccessControl/GreetingsController.java
+++ src/main/java/com/roleBasedAccessControl/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.roleBasedAccessControl;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String userEndPoint() {
+		return "hello, User!";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String adminEndPoint() {
+		return "hello, Admin!";
 	}
 }
